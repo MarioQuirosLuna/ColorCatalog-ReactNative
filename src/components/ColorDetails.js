3e185b7151a1deb1b2b0991f514e2f4df29247f5
MarioQuirosL <marioql.dev@gmail.com>
2023-03-10 12:46:56 -0600

    Add assets

--- src/components/ColorDetails.js
+++ src/components/ColorDetails.js
@@ -6,13 +6,16 @@ import Color from 'color'
 const ColorDetails = ({ route }) => {
     const { color: name } = route.params;
     const color = Color(name);
-    const textColor = { fontSize: 25, color: color.negate().toString() }
+    const textColor = { fontSize: 25, padding: 10, color: color.negate().toString() }
     return (
         <View style={[styles.container, { backgroundColor: name }]}>
             <Text style={textColor}>Color Name: {name}</Text>
-            <Text style={textColor}>RGB: {color.rgb().toString()}</Text>
-            <Text style={textColor}>HSL: {color.hsl().toString()}</Text>
-            <Text style={textColor}>Luminosity: {color.luminosity()}</Text>
+            <Text style={textColor}>RGB:</Text>
+            <Text style={textColor}>{color.rgb().toString()}</Text>
+            <Text style={textColor}>HSL:</Text>
+            <Text style={textColor}>{color.hsl().toString()}</Text>
+            <Text style={textColor}>Luminosity:</Text>
+            <Text style={textColor}>{color.luminosity()}</Text>
         </View>
     );
 }
